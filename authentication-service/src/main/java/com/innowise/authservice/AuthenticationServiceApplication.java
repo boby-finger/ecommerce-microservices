51900e6e30c70b53868e0725127160006f52f502
boby-finger <v.hulitsenka@gmail.com>
2026-07-29 14:42:56 +0200

    configs setup

--- authentication-service/src/main/java/com/innowise/authservice/AuthenticationServiceApplication.java
+++ authentication-service/src/main/java/com/innowise/authservice/AuthenticationServiceApplication.java
@@ -1,9 +1,12 @@
 package com.innowise.authservice;
 
+import com.innowise.authservice.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class AuthenticationServiceApplication {
 
 	public static void main(String[] args) {
---
+++ authentication-service/src/main/java/com/innowise/authservice/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.innowise.authservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/auth/**", "/oauth2/jwks").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()));
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
