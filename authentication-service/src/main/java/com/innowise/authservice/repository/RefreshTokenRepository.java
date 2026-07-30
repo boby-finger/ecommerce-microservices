903306cea2406714795c0c63fa515ad8a53d0c52
boby-finger <v.hulitsenka@gmail.com>
2026-07-30 15:10:19 +0200

    feat: add refresh token entity, repository and schema

---
+++ authentication-service/src/main/java/com/innowise/authservice/repository/RefreshTokenRepository.java
@@ -0,0 +1,14 @@
+package com.innowise.authservice.repository;
+
+import com.innowise.authservice.model.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+
+    Optional<RefreshToken> findByToken(String token);
+
+    void deleteByCredentialsId(UUID credentialsId);
+}
