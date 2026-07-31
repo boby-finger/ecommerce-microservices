93db3b69b9206d72835c5cac037fed9f98ca9c06
boby-finger <v.hulitsenka@gmail.com>
2026-07-31 21:35:32 +0200

    built new compose for auth-service

--- user-service/src/test/java/com/innowise/userservice/flow/PaymentCardFlowIntegrationTest.java
+++ user-service/src/test/java/com/innowise/userservice/flow/PaymentCardFlowIntegrationTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
@@ -14,6 +15,7 @@ import java.util.UUID;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@WithMockUser(roles = "ADMIN")
 class PaymentCardFlowIntegrationTest extends IntegrationTestBase {
 
     private static final String[] VALID_CARDS = {
--- user-service/src/test/java/com/innowise/userservice/flow/UserFlowIntegrationTest.java
+++ user-service/src/test/java/com/innowise/userservice/flow/UserFlowIntegrationTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
@@ -13,6 +14,7 @@ import java.util.UUID;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@WithMockUser(roles = "ADMIN")
 class UserFlowIntegrationTest extends IntegrationTestBase {
 
     @Autowired
